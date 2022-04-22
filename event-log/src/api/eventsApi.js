63dd06684bffa1112a13c35eaf74f577b8aac2ae
Ziqiang Cheng <chengziqiang@gmail.com>
2022-04-22 18:21:43 -0400

    fetch replaced by axios

--- event-log/src/api/eventsApi.js
+++ event-log/src/api/eventsApi.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import axios from "axios";
 const EVENTS_API = "https://601caf791a9c220017060c02.mockapi.io/api/v1/Events";
 const HOSTS_API = "https://601caf791a9c220017060c02.mockapi.io/api/v1/Hosts";
 
@@ -11,9 +12,9 @@ export const getData = async (date, eventId) => {
       : eventId
       ? `?eventId=${eventId}`
       : "";
-    const responseEvent = await fetch(`${EVENTS_API}${addon}`);
+    const responseEvent = await axios.get(`${EVENTS_API}${addon}`);
     const events = await responseEvent.json();
-    const responseHost = await fetch(HOSTS_API);
+    const responseHost = await axios.get(HOSTS_API);
     const hosts = await responseHost.json();
     return [events, hosts];
   } catch (err) {
@@ -27,13 +28,14 @@ export const postData = async (inputData) => {
     date: inputData.date.format().slice(0, -6),
   };
   try {
-    await fetch(EVENTS_API, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    // await fetch(EVENTS_API, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(payload),
+    // });
+    await axios.post("EVENTS_API", payload);
   } catch (err) {
     console.error(err.message);
   }
@@ -46,13 +48,14 @@ export const postRating = async (id, rate) => {
   };
   const url = `${EVENTS_API}/${id}/rates`;
   try {
-    await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(rating),
-    });
+    // await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(rating),
+    // });
+    await axios.post(url, rating);
   } catch (err) {
     console.error(err.message);
   }
@@ -67,13 +70,14 @@ export const postAttendee = async (id, name, photoURL, email) => {
   };
   const url = `${EVENTS_API}/${id}/Attendees`;
   try {
-    await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(attendee),
-    });
+    // await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(attendee),
+    // });
+    await axios.post(url, attendee);
   } catch (err) {
     console.error(err.message);
   }
@@ -96,13 +100,14 @@ export const postComment = async (
   };
   const url = `${EVENTS_API}/${id}/comments`;
   try {
-    await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newComment),
-    });
+    // await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newComment),
+    // });
+    await axios.post(url, newComment);
   } catch (err) {
     console.error(err.message);
   }
