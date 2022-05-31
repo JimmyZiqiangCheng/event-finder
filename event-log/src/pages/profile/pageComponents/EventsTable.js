fb6dcf0f1610f9419fee1af63ced56c09035f45a
Jimmy Cheng <jimmycheng@Jimmys-MacBook-Pro.local>
2022-05-31 15:43:51 -0400

    profile events table updated

--- event-log/src/pages/profile/pageComponents/EventsTable.js
+++ event-log/src/pages/profile/pageComponents/EventsTable.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "antd/dist/antd.css";
 import { Table } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { useAuth } from "../../../utils/customHooks";
+import { loadEvents } from "../../../redux/actions/eventActions";
+import { loadHosts } from "../../../redux/actions/hostActions";
+import { getData } from "../../../api/eventsApi";
 
 const columns = [
   {
@@ -23,16 +28,27 @@ const columns = [
   },
 ];
 
-const data = [
-  {
-    key: "1",
-    title: "basketball",
-    category: "sports",
-    date: "2022-02-24",
-  },
-];
-
 function EventsTable() {
+  const { currentUser } = useAuth();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const loadData = async () => {
+      const [events, hosts] = await getData();
+      dispatch(loadEvents(events));
+      dispatch(loadHosts(hosts));
+    };
+    loadData();
+  }, []);
+  const events = useSelector((state) => state.events);
+  const participatedEvents = events.filter((event) =>
+    event.attendees.map((a) => a.email).includes(currentUser.email)
+  );
+  const data = participatedEvents.map((e) => ({
+    key: e.eventId,
+    title: e.title,
+    category: e.category,
+    date: e.date,
+  }));
   return <Table columns={columns} dataSource={data} pagination={false} />;
 }
 
