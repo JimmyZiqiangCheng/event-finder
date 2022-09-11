5ddbf4a986eaee89dad466ae684851981df5f972
Jimmy Cheng <jimmycheng@Jimmys-MacBook-Pro.local>
2022-09-11 12:02:17 -0600

    event image lazy loading implemented

---
+++ src/components/eventItem/EventItem.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { List, Space, Rate, Avatar } from "antd";
+import { MessageOutlined, LikeOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+
+const EventItem = ({ item, hosts, defaultAvatar }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  const IconText = ({ icon, text }) => (
+    <Space>
+      {React.createElement(icon)}
+      {text}
+    </Space>
+  );
+  const createRating = (item) => {
+    const rate =
+      item.rate.length !== 0
+        ? Math.floor(
+            item.rate.map((ele) => ele.rate).reduce((sum, a) => a + sum) /
+              item.rate.length
+          )
+        : 0;
+    return <Rate allowHalf disabled defaultValue={rate} />;
+  };
+  const countLike = (item) => {
+    return item.rate.length !== 0
+      ? item.rate.map((ele) => ele.rate).filter((ele) => ele === 5).length
+      : 0;
+  };
+  const createAvatar = (hosts, item, defaultAvatar) => {
+    const arr =
+      hosts.length !== 0
+        ? hosts.filter((h) => h.eventId === item.eventId)
+        : null;
+    let source = defaultAvatar;
+    if (arr && arr.length > 0) {
+      source = arr[0].photoUrl;
+    }
+    return <Avatar src={source} />;
+  };
+
+  return (
+    <Link to={`/events/${item.eventId}`} ref={ref}>
+      <List.Item
+        key={item.title}
+        data-testid="row"
+        actions={[
+          <Space>{createRating(item)}</Space>,
+          <IconText
+            icon={LikeOutlined}
+            text={countLike(item)}
+            key="list-vertical-like-o"
+          />,
+          <IconText
+            icon={MessageOutlined}
+            text={item.comments.length}
+            key="list-vertical-message"
+          />,
+        ]}
+        extra={
+          inView && (
+            <img
+              className="event-img"
+              width={272}
+              alt=""
+              src={`./img/${item.category}.jpg`}
+            />
+          )
+        }
+      >
+        <List.Item.Meta
+          avatar={createAvatar(hosts, item, defaultAvatar)}
+          title={item.title}
+          description={item.description}
+        />
+        {item.content}
+      </List.Item>
+    </Link>
+  );
+};
+
+export default EventItem;
--- src/pages/events/pageComponents/EventList.js
+++ src/pages/events/pageComponents/EventList.js
@@ -1,50 +1,13 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { List, Avatar, Space, Rate } from "antd";
-import { MessageOutlined, LikeOutlined } from "@ant-design/icons";
+import { List } from "antd";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
-const IconText = ({ icon, text }) => (
-  <Space>
-    {React.createElement(icon)}
-    {text}
-  </Space>
-);
-const defaultAvatar = "https://joeschmoe.io/api/v1/random";
+import EventItem from "../../../components/eventItem/EventItem";
 
 function EventList(props) {
   const events = useSelector((state) => state.events);
   const hosts = useSelector((state) => state.hosts);
-
-  const createAvatar = (hosts, item, defaultAvatar) => {
-    const arr =
-      hosts.length !== 0
-        ? hosts.filter((h) => h.eventId === item.eventId)
-        : null;
-    let source = defaultAvatar;
-    if (arr && arr.length > 0) {
-      source = arr[0].photoUrl;
-    }
-    return <Avatar src={source} />;
-  };
-
-  const createRating = (item) => {
-    const rate =
-      item.rate.length !== 0
-        ? Math.floor(
-            item.rate.map((ele) => ele.rate).reduce((sum, a) => a + sum) /
-              item.rate.length
-          )
-        : 0;
-    return <Rate allowHalf disabled defaultValue={rate} />;
-  };
-
-  const countLike = (item) => {
-    return item.rate.length !== 0
-      ? item.rate.map((ele) => ele.rate).filter((ele) => ele === 5).length
-      : 0;
-  };
+  const defaultAvatar = "https://joeschmoe.io/api/v1/random";
 
   return (
     <>
@@ -59,40 +22,11 @@ function EventList(props) {
           }}
           dataSource={events}
           renderItem={(item) => (
-            <Link to={`/events/${item.eventId}`}>
-              <List.Item
-                key={item.title}
-                data-testid="row"
-                actions={[
-                  <Space>{createRating(item)}</Space>,
-                  <IconText
-                    icon={LikeOutlined}
-                    text={countLike(item)}
-                    key="list-vertical-like-o"
-                  />,
-                  <IconText
-                    icon={MessageOutlined}
-                    text={item.comments.length}
-                    key="list-vertical-message"
-                  />,
-                ]}
-                extra={
-                  <img
-                    className="event-img"
-                    width={272}
-                    alt=""
-                    src={`./img/${item.category}.jpg`}
-                  />
-                }
-              >
-                <List.Item.Meta
-                  avatar={createAvatar(hosts, item, defaultAvatar)}
-                  title={item.title}
-                  description={item.description}
-                />
-                {item.content}
-              </List.Item>
-            </Link>
+            <EventItem
+              item={item}
+              hosts={hosts}
+              defaultAvatar={defaultAvatar}
+            />
           )}
         />
       )}
